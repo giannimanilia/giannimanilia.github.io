557273e2ef1a122abc7252ec62551969c6bb3791
Gianni Antonio Manilia <giannimanilia@gmail.com>
2020-06-11 11:49:13 +0200

    Fixed projects' images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,68 +1,27 @@
 import React, { Component } from 'react';
-export default class Porfolio extends Component {
-  render() {
-    return (
-      <section id="portfolio">
-        <div className="row">
-          <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Works.</h1>
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              <div className="columns portfolio-item">
-                <div className="item-wrap">
-                  <a href="#modal-01">
-                    <img src={require("../assets/uniapp-usb.png")} className="item-img" alt="uniapp-usb"/>
-                    <div className="overlay">
-                      <div className="portfolio-item-meta">
-                        <h5>UniApp USB</h5>
-                        <p>DESCRIPTION</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="columns portfolio-item">
-                <div className="item-wrap">
-                  <a href="#modal-01">
-                    <img src={require("../assets/sudoku-solver.png")} className="item-img" alt="sudoku-solver"/>
-                    <div className="overlay">
-                      <div className="portfolio-item-meta">
-                        <h5>Sudoku Solver</h5>
-                        <p>DESCRIPTION</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="columns portfolio-item">
-                <div className="item-wrap">
-                  <a href="#modal-01">
-                    <img src={require("../assets/pokemon-list.png")} className="item-img" alt="pokemon-list"/>
-                    <div className="overlay">
-                      <div className="portfolio-item-meta">
-                        <h5>Pokemon List</h5>
-                        <p>DESCRIPTION</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="columns portfolio-item">
-                <div className="item-wrap">
-                  <a href="#modal-01">
-                    <img src={require("../assets/portfolio.png")} className="item-img" alt="portfolio"/>
-                    <div className="overlay">
-                      <div className="portfolio-item-meta">
-                        <h5>Portfolio</h5>
-                        <p>DESCRIPTION</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
+import {Row, Column, Project} from './Portfolio.style';
+
+export default class Portfolio extends Component {
+	render() {
+		return (
+			<Row>
+				<Column>
+					<Project>
+						<img src={require("../assets/uniapp-usb.png")}/>
+					</Project>
+          <Project>
+            <img src={require("../assets/sudoku-solver.png")}/>
+          </Project>
+				</Column>
+				<Column>
+          <Project>
+            <img src={require("../assets/pokemon-list.png")}/>
+          </Project>
+          <Project>
+            <img src={require("../assets/portfolio.png")}/>
+          </Project>
+				</Column>
+			</Row>
+		);
+	}
 }
---
+++ src/components/Portfolio.style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Row = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	padding: 0 4px;
+`;
+
+export const Column = styled.div`
+	flex: 50%;
+	max-width: 50%;
+	padding: 0 4px;
+
+	img {
+		margin-top: 8px;
+		vertical-align: middle;
+		width: 100%;
+
+		&:hover{
+			opacity: 0.5;
+		}
+	}
+
+	@media screen and (max-width: 800px){
+		& {
+			flex: 50%;
+			max-width: 50%;
+		}
+	}
+
+	@media screen and (max-width: 600px){
+		& {
+			flex: 100%;
+			max-width: 100%;
+		}
+	}
+`;
+
+export const Project = styled.div`
+`;
