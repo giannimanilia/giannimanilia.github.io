3a2ca1c0645ddd450bacfd48553f450e76775607
gianni <giannimanilia@gmail.com>
2025-11-09 19:14:00 +0100

    Arrange projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import '../css/projects.css';
 
 export default class Projects extends Component {
 	render() {
-	    let projectsJson = this.props.projectsJson;
+	    const projects = this.props.projectsJson.projects || [];
 		return (
 			<section className="projects" id="projects">
                 <h2 className="heading">
@@ -12,34 +12,32 @@ export default class Projects extends Component {
       
                 <div className="box-container">
                     {
-                        projectsJson.projects && projectsJson.projects.map((project) => {
-                            return(
-                                <div className={`box tilt ${!project.links.view && !project.links.code ? "no-hover" : ""}`} key={project.id}>
-                                    <img src={project.image} alt={project.name} />
-                                    <div className="content">
-                                        <div className="tag">
-                                            <h3>{project.name}</h3>
-                                        </div>
-                                        <div className="desc">
-                                            <p>{project.desc}</p>
-                                            <div className="btns">
-                                                {project.links.view != null &&
-                                                    <a href={project.links.view} className="btn" target="_blank" rel="noopener noreferrer">
-                                                        <i className="fas fa-eye"></i>&nbsp;View
-                                                    </a>
-                                                }
-                                                {project.links.code != null &&
-                                                    <a href={project.links.code} className="btn" target="_blank" rel="noopener noreferrer">
-                                                        <i className="fas fa-code"></i>&nbsp;Code
-                                                    </a>
-                                                }
-                                            </div>
+                        projects.map((project) => (
+                            <div key={project.id} className={`box tilt ${!project.links.view && !project.links.code ? 'no-hover' : ''}`}>
+                                <img src={project.image} alt={project.name} />
+                                <div className="content">
+                                    <div className="tag">
+                                        <h3>{project.name}</h3>
+                                    </div>
+                                    <div className="desc">
+                                        <p>{project.desc}</p>
+                                        <div className="btns">
+                                            {project.links.view &&
+                                                <a href={project.links.view} className="btn" target="_blank" rel="noopener noreferrer">
+                                                    <i className="fas fa-eye"></i>&nbsp;View
+                                                </a>
+                                            }
+                                            {project.links.code &&
+                                                <a href={project.links.code} className="btn" target="_blank" rel="noopener noreferrer">
+                                                    <i className="fas fa-code"></i>&nbsp;Code
+                                                </a>
+                                            }
                                         </div>
                                     </div>
                                 </div>
-                            )
-                        })
-                    }
+                            </div>
+                        )
+                    )}
                 </div>
             </section>
 		);
