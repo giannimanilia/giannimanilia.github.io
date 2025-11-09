e50fd64e9ca90cfae625d567b75d16ee1876392a
gianni <giannimanilia@gmail.com>
2025-11-09 19:14:31 +0100

    Code style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,8 +36,8 @@ export default class Projects extends Component {
                                     </div>
                                 </div>
                             </div>
-                        )
-                    )}
+                        ))
+                    }
                 </div>
             </section>
 		);
